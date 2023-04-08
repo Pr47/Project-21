0c54c90fcf2d02db90b074988c4f41876eb1b1c8
icey <icey@icey.tech>
2023-04-08 10:58:45 +0800

    rename

--- src/compiler/parse/test.rs
+++ src/compiler/parse/test.rs
@@ -1,19 +1,19 @@
 use crate::compiler::lex::tokenize;
 
 #[test]
-fn test_simple() {
+fn test_lex_simple() {
     let tokens = tokenize("void main() {}").unwrap();
     dbg!(tokens);
 }
 
 #[test]
-fn test_interp() {
+fn test_lex_interp() {
     let tokens = tokenize(include_str!("./test_resc/interp.bis")).unwrap();
     dbg!(tokens);
 }
 
 #[test]
-fn test_spacium() {
+fn test_lex_spacium() {
     let tokens = tokenize(include_str!("./test_resc/spacium.bis")).unwrap();
     dbg!(tokens);
 }
